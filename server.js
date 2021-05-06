05614eb2278f6c1b57f363f13981d56cde55889e
Kasra Karami <kasra.karami.kk@gmail.com>
2021-05-07 02:49:12 +0430

    fix port

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ app.use(express.static("./public"));
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
-app.listen(3000, () =>
-  console.log("Server is running on http://localhost:3000")
-);
+const port = parseInt(process.env.PORT) || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
